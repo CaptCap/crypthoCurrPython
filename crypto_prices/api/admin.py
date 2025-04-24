60181ba939c21f702511b31bb02320da9844e848
CaptCap <112699343+CaptCap@users.noreply.github.com>
2025-04-24 16:30:24 +0300

    function to get data from coingecko added to views

--- crypto_prices/api/admin.py
+++ crypto_prices/api/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from  .models import Cryptocurrency
+
+admin.site.register(Cryptocurrency)
+
 
 # Register your models here.
--- crypto_prices/api/views.py
+++ crypto_prices/api/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+def get_cryptocurrency_data(coin_id):
+    url = f"https://api.coingecko.com/api/v3/simple/price?ids={coin_id}&vs_currencies=usd"
+    response = requests.get(url)
+    data = response.json()
+    return data[coin_id]['usd']
